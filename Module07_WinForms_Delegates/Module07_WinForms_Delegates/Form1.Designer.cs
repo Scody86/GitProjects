672b748dc9e4748230e45c84fba0600f2edc38b4
Scott Cody <scody6535@gmail.com>
2017-04-28 23:55:41 -0500

    Module07_Timer

---
+++ Module07_WinForms_Delegates/Module07_WinForms_Delegates/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Module07_WinForms_Delegates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblFirst = new System.Windows.Forms.Label();
+            this.lblSecond = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gray;
+            this.button1.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1.Location = new System.Drawing.Point(12, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 158);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click Me!!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblFirst
+            // 
+            this.lblFirst.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblFirst.Font = new System.Drawing.Font("Magneto", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirst.ForeColor = System.Drawing.Color.MediumBlue;
+            this.lblFirst.Location = new System.Drawing.Point(211, 73);
+            this.lblFirst.Name = "lblFirst";
+            this.lblFirst.Size = new System.Drawing.Size(272, 55);
+            this.lblFirst.TabIndex = 1;
+            // 
+            // lblSecond
+            // 
+            this.lblSecond.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSecond.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSecond.ForeColor = System.Drawing.Color.OrangeRed;
+            this.lblSecond.Location = new System.Drawing.Point(211, 176);
+            this.lblSecond.Name = "lblSecond";
+            this.lblSecond.Size = new System.Drawing.Size(272, 55);
+            this.lblSecond.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 302);
+            this.Controls.Add(this.lblSecond);
+            this.Controls.Add(this.lblFirst);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "My Delegate Application";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblFirst;
+        private System.Windows.Forms.Label lblSecond;
+    }
+}
+
---
+++ Module07_WinForms_Delegates/Module07_WinForms_Delegates/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module07_WinForms_Delegates
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += Button_myOtherClick;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblFirst.Text = "Event was triggered!";
+            
+        }
+
+        private void Button_myOtherClick(object sender, EventArgs e)
+        {
+            lblSecond.Text = "This one worked too!!";
+        }
+    }
+}
