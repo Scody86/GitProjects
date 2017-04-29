abb5ac30542b48afcfd0b3347cb1a4e0b706de6f
Scott Cody <scody6535@gmail.com>
2017-04-29 00:11:49 -0500

    Module07_Delegates

---
+++ Module07_Delegates/Module07_Delegates/Program.cs
@@ -0,0 +1,31 @@
+﻿// Scott Cody
+// Advanced C# Thu 8 - 11
+// Module07 Delegate Example
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module07_Delegates
+{
+    public delegate int addNumsDelegate(int num1, int num2);
+
+    class Program
+    {
+        public static int addNums(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        static void Main(string[] args)
+        {
+            addNumsDelegate del = new addNumsDelegate(addNums);
+            int solution = del(2, 3);
+            Console.WriteLine("The solution is {0}.", solution);
+
+            Console.Read();
+        }
+    }
+}
